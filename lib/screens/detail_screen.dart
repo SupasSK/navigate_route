e57f148bbe3f19f81e836c5e072ec6a346fe97e6
SupasSK <oppo.suphatsaraporn@gmail.com>
2025-09-07 21:53:11 +0700

    refactor: use provider in navigate_route

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/message_provider.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key});
 
-  static const routeName = '/detail';
-
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
-    final itemId = args?['id'] ?? 'N/A';
-    final title  = args?['title'] ?? 'No title';
+    final messageFromHome =
+        context.watch<MessageProvider>().messageForDetail ?? 'N/A';
 
     return Scaffold(
-      appBar: AppBar(title: Text('Detail: $itemId')),
+      appBar: AppBar(title: const Text('Detail Screen')),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text('Title: $title'),
-            const SizedBox(height: 20),
-
-            ElevatedButton(
-              onPressed: () => Navigator.pop(context, 'DONE:$itemId'),
-              child: const Text('ส่งผลลัพธ์และย้อนกลับ'),
+            Text(
+              messageFromHome,
+              textAlign: TextAlign.center,
             ),
-
-            const SizedBox(height: 12),
-            OutlinedButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('ย้อนกลับ (ไม่ส่งค่า)'),
+            const SizedBox(height: 24),
+            FilledButton(
+              onPressed: () {
+                context
+                    .read<MessageProvider>()
+                    .setMessageForHome('this is data from detail page');
+                Navigator.pop(context);
+              },
+              child: const Text('Go Back'),
             ),
           ],
         ),
